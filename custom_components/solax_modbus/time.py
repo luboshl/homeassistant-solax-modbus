32bd212daa026d7f2377485bd9c606ef30a37ef3
wills106 <william.f.swann@googlemail.com>
2023-05-25 09:05:08 +0100

    Update time.py
    Correct Typo

--- custom_components/solax_modbus/time.py
+++ custom_components/solax_modbus/time.py
@@ -1,6 +1,6 @@
 from .const import ATTR_MANUFACTURER, DOMAIN, CONF_MODBUS_ADDR, DEFAULT_MODBUS_ADDR
 from .const import WRITE_DATA_LOCAL, WRITE_MULTISINGLE_MODBUS, WRITE_SINGLE_MODBUS, TMPDATA_EXPIRY
-from homeassistant.components.timme import PLATFORM_SCHEMA, TimeEntity
+from homeassistant.components.time import PLATFORM_SCHEMA, TimeEntity
 from homeassistant.const import CONF_NAME
 from homeassistant.core import callback
 from typing import Any, Dict, Optional
